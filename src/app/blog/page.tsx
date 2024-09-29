369483780974e735af2630ac4195b8b7086d3755
zeyad tamer <zeyadtamer101@gmail.com>
2024-09-30 00:35:28 +0300

    adding adsense script

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import Hero from './_components/Hero'
-import Features from './_components/Features'
 import Articles from './_components/Articles'
-import AdComponent from '../_layout/AdComponent'
 
 function BlogPage() {
     return (
         <>
             <Hero/>
-            {/* <Features/> */}
             <Articles/>
-            {/* <AdComponent/> */}
         </>
     )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({children}:RootLayout) {
     return (
         <html lang="en">
             <head>
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4205214329068892" crossOrigin="anonymous"></script>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4412279983219767" crossOrigin="anonymous"></script>                
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
                 <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap" rel="stylesheet"/>
